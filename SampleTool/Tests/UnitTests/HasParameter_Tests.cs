f3cf3bcf3ea18297b9cf5872d4e83663a254902c
MOTTMAC\cam82478 <luke.campbell@mottmac.com>
2017-08-07 08:50:57 +0100

    Fixed for my local project so that everything works.

--- SampleTool/Tests/UnitTests/HasParameter_Tests.cs
+++ SampleTool/Tests/UnitTests/HasParameter_Tests.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
-using Autodesk.Revit.DB;
 using SampleTool.Helper;
+using Autodesk.Revit.DB;
 
 namespace SampleTool.Tests.UnitTests
 {
@@ -38,5 +38,13 @@ namespace SampleTool.Tests.UnitTests
 			IParameterHelper parameterHelper = new ParameterHelper(document);
 			Assert.That(parameterHelper.HasParameter("Project Name"), Iz.True);
 		}
-	}
+
+        [Test]
+        public void ShouldNotHaveParameter2_Test()
+        {
+            Document document = Helpers.GeneralHelper.ActiveUIDocument.Document;
+            IParameterHelper parameterHelper = new ParameterHelper(document);
+            Assert.That(parameterHelper.HasParameter("SomeParameterThatNormallyDoesnotExist"), Iz.False);
+        }
+    }
 }
